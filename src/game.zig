e408cbd32a111fa78dc73a5db6862da176c674f8
Halfdan <halfdansc@gmail.com>
2025-06-30 13:24:59 +0200

    cleaned up game.zig

--- src/game.zig
+++ src/game.zig
@@ -10,6 +10,7 @@ const ProjectileManager = @import("projectile/projectile.zig").ProjectileManager
 const EnemyManager = @import("enemy/enemyManager.zig").EnemyManager;
 const GameState = @import("utils/gameState.zig").GameState;
 const textureLoader = @import("utils/textureLoader.zig");
+const UI = @import("ui/ui.zig").UI;
 const gameConst = @import("utils/constants/gameConstants.zig");
 
 // game state handlers
@@ -25,6 +26,7 @@ pub const Game = struct {
     current_state: GameState,
     playing_state: PlayingState,
     game_over_state: GameOverState,
+    ui: UI,
     allocator: std.mem.Allocator,
 
     pub fn init(allocator: std.mem.Allocator) !Game {
@@ -36,6 +38,7 @@ pub const Game = struct {
         const aim_circle = AimCircle.init();
         const projectile_manager = ProjectileManager.init(allocator);
         const enemy_manager = EnemyManager.init(allocator);
+        const ui = UI.init(allocator);
 
         return Game{
             .player = player,
@@ -46,6 +49,7 @@ pub const Game = struct {
             .current_state = GameState.playing, // TODO: when menu is added start at the start menu
             .playing_state = PlayingState.init(),
             .game_over_state = GameOverState.init(),
+            .ui = ui,
             .allocator = allocator,
         };
     }
@@ -54,6 +58,7 @@ pub const Game = struct {
         self.textures.deinit();
         self.projectile_manager.deinit();
         self.enemy_manager.deinit();
+        self.ui.deinit();
         rl.CloseWindow();
     }
 
@@ -126,81 +131,25 @@ pub const Game = struct {
                 rl.ClearBackground(rl.SKYBLUE);
             },
             .game_over => {
-                self.game_over_state.draw();
+                self.ui.drawGameOverUI(self.game_over_state.selected_option);
             },
         }
     }
 
     fn drawGameplay(self: *Game) void {
         rl.ClearBackground(rl.SKYBLUE);
+        rl.ShowCursor();
+
+        // draw game objects
         self.player.draw(self.textures.player);
         self.aim_circle.draw(self.player.position);
         self.projectile_manager.draw(self.textures.projectile);
         self.enemy_manager.draw(self.textures.enemy);
 
-        // draw system UI
-        self.playing_state.drawUI();
+        // draw the ui elements
+        self.ui.drawGameOverUI(&self.player, &self.playing_state.round_manager);
 
-        // TODO: move this to a dedicated ui file
-        const fps = rl.GetFPS();
-        const fps_text = std.fmt.allocPrint(self.allocator, "FPS: {d}", .{ fps, }) catch "FPS: ?";
-        defer self.allocator.free(fps_text);
-        const fps_text_c = self.allocator.dupeZ(u8, fps_text) catch return;
-        defer self.allocator.free(fps_text_c);
-
-        const text_width = rl.MeasureText(fps_text_c.ptr, 20);
-        rl.DrawText(fps_text_c, gameConst.WINDOW_WIDTH - text_width - 20, 20, 20, rl.WHITE);
-
-        // draw health hearts
-        self.drawHealthHearts();
-    }
-
-    // TODO: MOVE THIS TO A DEDICATED UI FILE
-    fn drawHealthHearts(self: *Game) void {
-        // TODO: find a heart sprite
-        // for now it will be diamonds
-        const heart_size = 30;
-        const heart_spacing = 40;
-        const total_width = (gameConst.DEFAULT_PLAYER_MAX_HEALTH * heart_spacing) - (heart_spacing - heart_size);
-        const start_x = (gameConst.WINDOW_WIDTH / 2) - (total_width / 2);
-        const y_pos = 20;
-
-        var i: u32 = 0;
-        while (i < gameConst.DEFAULT_PLAYER_MAX_HEALTH) : (i += 1) {
-            const x_pos = start_x + (i * heart_spacing);
-
-            // determine heart color lose hearts from right to left
-            const heart_lost = gameConst.DEFAULT_PLAYER_MAX_HEALTH - self.player.current_health;
-            const is_lost_heart = i >= (gameConst.DEFAULT_PLAYER_MAX_HEALTH - heart_lost);
-            const heart_color = if (is_lost_heart) rl.BLACK else rl.RED;
-
-            // draw diamond for now
-            self.drawHeart(@intCast(x_pos), @intCast(y_pos), @intCast(heart_size), heart_color);
-        }
-    }
-
-    // TODO: MOVE THIS TO A DEDICATED UI FILE
-    fn drawHeart(self: *Game, x: i32, y: i32, size: i32, color: rl.Color) void {
-        _ = self; // unused
-
-        // simple heart shape using circles and triangles????
-        const half_size = @divTrunc(size, 2);
-        const quarter_size = @divTrunc(size, 4);
-
-        // top two circles
-        rl.DrawCircle(x + quarter_size, y + quarter_size, @floatFromInt(quarter_size), color);
-        rl.DrawCircle(x + (3 * quarter_size), y + quarter_size, @floatFromInt(quarter_size), color);
-
-        // bottom triangle
-        rl.DrawTriangle(
-            rl.Vector2{ .x = @floatFromInt(x), .y = @floatFromInt(y + half_size), },
-            rl.Vector2{ .x = @floatFromInt(x + size), .y = @floatFromInt(y + half_size), },
-            rl.Vector2{ .x = @floatFromInt(x + half_size), .y = @floatFromInt(y + size), },
-            color
-        );
-
-        // fill the middle rectangle
-        rl.DrawRectangle(x + quarter_size, y + quarter_size, half_size, quarter_size, color);
+        // TODO: optional make a debug ui overlay
     }
 
     fn transitionToState(self: *Game, new_state: GameState) void {
